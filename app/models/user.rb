13aa586bffaa9498414009fed043a17bb82ce4d2
jimmy <jimewilson87@gmail.com>
2020-11-12 02:31:12 -0600

    updates parameters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :user_name, presence: true, uniqueness: true, length: { maximum 20 }
+  validates :user_name, presence: true, uniqueness: true, length: { maximum: 20 }
   validates :password, length: { in: 8..20 }
   validates :email, presence: true, uniqueness: true
   validates :address, presence: true
